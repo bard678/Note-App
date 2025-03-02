1a8f9d7fa134dd256b81e3490982331b3cd2ae29
bard678 <bard76217@gmail.com>
2025-03-01 22:59:41 -0800

    Edit on Test.kt

--- app/src/main/java/com/example/myapplication/Test.kt
+++ app/src/main/java/com/example/myapplication/Test.kt
@@ -3,7 +3,7 @@ package com.example.myapplication
 import kotlinx.coroutines.*
 suspend fun fetchData(): String {
     delay(2000L)  // Simulate a network delay
-    return "Data fetched"
+    return "Data fetched !!"
 }
 fun main() = runBlocking { // This creates a coroutine scope for the main thread
     launch {  // Start a new coroutine
